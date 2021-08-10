405bbb51e0fee795b7d3fa5d75b1d6806d0f5160
Vanderllee <vanderllee@gmail.com>
2021-08-10 18:14:14 -0300

    changed index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className={
+      ` h-screen
+        flex justify-center items-center  
+        bg-gradient-to-r from-green-500 to-blue-500     `
+    }
+    >
       <h1>Admin</h1>
     </div>
   )
