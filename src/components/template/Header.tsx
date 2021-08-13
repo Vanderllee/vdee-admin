f58f0a16a85d5ac55a536c4e9abfd321bbdac2c1
Vanderllee <vanderllee@gmail.com>
2021-08-13 17:41:55 -0300

    created btn for handle theme

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,3 +1,5 @@
+import useAppData from '../../data/hook/useAppData';
+import SwitchThmeBtn from './SwitchThemeBtn';
 import Title from './Title';
 
 type HeaderProps = {
@@ -6,9 +8,18 @@ type HeaderProps = {
 }
 
 export default function Header({ title, subtitle }: HeaderProps) {
+
+        const {handleTheme, theme } = useAppData();
+
     return (
-        <div>          
+        <div className={`flex`}>          
             <Title title={title} subtitle={subtitle}/>
+
+            <div className={`
+                 flex flex-grow justify-end 
+            `}>
+                <SwitchThmeBtn theme={ theme } switchTheme={ handleTheme }/>
+            </div>
         </div>
     )
 }
--- src/pages/notifiers.tsx
+++ src/pages/notifiers.tsx
@@ -1,14 +1,12 @@
 import Layout from "../components/template/Layout";
-import useAppData from "../data/hook/useAppData";
+
 
 export default function Notificacoes() {
 
-    //const dataContext = useAppData()
-    const { handleTheme } = useAppData();
   return (
    <Layout title={'Notificações'} subtitle={'Página de notificações'}>
      
-     <button onClick={handleTheme}> trocar de Tema! </button>
+     <h1>Noficações</h1>
 
    </Layout>
   )
